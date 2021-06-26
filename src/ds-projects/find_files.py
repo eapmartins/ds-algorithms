5f24f41447dcd2e7313e749dc345dad9a6d0058a
Edson Martins <aekmartins@gmail.com>
2021-06-26 14:40:18 -0300

    add helper function to keep single array of paths

--- src/ds-projects/find_files.py
+++ src/ds-projects/find_files.py
@@ -17,22 +17,34 @@ def find_files(suffix, path):
        a list of paths
     """
 
-    files = os.listdir(path)
+    if suffix is None or path == "" or path is None:
+        return []
+        
     paths = []
 
-    for file in files:
+    def _find_files_helper(suffix, path):
 
-        full_path = path + file
+        for file in os.listdir(path):
 
-        if file.endswith(suffix):
-            paths.append(full_path)
+            full_path = os.path.join(path, file)
 
-        if os.path.isdir(full_path):
-            file = find_files(suffix, full_path + "/")
-            paths.append(file)
+            if not os.path.isdir(full_path) and file.endswith(suffix):
+                paths.append(full_path)
+            elif os.path.isdir(full_path):
+                _find_files_helper(suffix, full_path)
 
-    return paths
+        return sorted(paths)
+    
+    return _find_files_helper(suffix, path)
 
+"""
+/Users/eapmartins/temp/file1.py
+/Users/eapmartins/temp/file2.py
+/Users/eapmartins/temp/folder2/file3.py
+/Users/eapmartins/temp/folder3/file1.java
+"""
 
-print(find_files(".py", "/Users/eapmartins/temp/"))
-
+assert find_files(".py", "") == []
+assert find_files(".py", None) == []
+assert len(find_files(".py", "/Users/eapmartins/temp/")) == 3
+assert find_files(".py", "/Users/eapmartins/temp/") == ["/Users/eapmartins/temp/file1.py", "/Users/eapmartins/temp/file2.py", "/Users/eapmartins/temp/folder2/file3.py"]
